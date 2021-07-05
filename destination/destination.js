a4cb423bfbba199d76baa1a4697566712a900493
Đặng Thùy Trang <dangthuytrang98@gmail.com>
2021-07-05 09:31:48 +0700

    sửa destination

--- destination/destination.js
+++ destination/destination.js
@@ -80,15 +80,34 @@ fetch(url)
         // logic code
         for (const p of data) {
             places.innerHTML += `
-            <div class="box">
-            <div class="img1">
-                <a href="#"><img src="${p.img}" style=width:170px;height:120px;></a>
+            <button type="button" class="box" data-toggle="modal" data-target="#${p.id}">
+                <div class="img1">
+                    <a href="#"><img src="${p.img}" style=width:170px;height:120px;></a>
+                </div>
+                <div class="adcontent">
+                    <a href="#">
+                    ${p.name}<br>
+                </a>
+                </div>
+            </button>
+            <div class="modal fade" id="${p.id}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
+                    <div class="modal-dialog" role="document">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title" id="exampleModalLongTitle">${p.name}</h5>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <div class="modal-body">
+                                <img src="${p.img}" style="width: 450px; height: 300px;">
+                                <br><br> ${p.des} </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                            </div>
+                        </div>
+                    </div>
             </div>
-            <div class="adcontent">
-                <a href="#">${p.name}<br>
-            </a>
-            </div>
-        </div>
             `
         }
     })
